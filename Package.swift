edac2fb31bf69908665eb4b6a97190da5d300cf3
Jia Jia <33975477+jiayang007@users.noreply.github.com>
2023-10-11 17:45:25 +0800

    Update Package.swift
    11

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+       // .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.19.0"),
     ],
     targets: [
